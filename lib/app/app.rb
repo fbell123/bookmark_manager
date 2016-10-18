939805085d3b3d886ad39bc125f194fd5584ed5a
LMCMLJ <mjcalvey2@gmail.com>
2016-10-18 10:13:46 +0100

    Rehashing feature tests, adding full db integration

--- lib/app/app.rb
+++ lib/app/app.rb
@@ -5,6 +5,11 @@ class BookmarkManager < Sinatra::Base
     'Hello BookmarkManager!'
   end
 
+  get '/links' do
+    @links = Link.all
+    erb :'links/index'
+  end
+
   # start the server if ruby file executed directly
   run! if app_file == $0
 end
--- spec/features/bookmark_spec.rb
+++ spec/features/bookmark_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
-require 'link'
+require './lib/app/models/link'
 
-RSpec.feature "link display", :type => :feature do
-  Link.create( id: 1, link: 'http://google.com', title: 'google')
+RSpec::feature "link display", :type => :feature do
+  Link.create(id: 5, url: 'http://google.com', title: 'google')
 
-  visit '/links'
+    scenario 'be able to view stored links' do
+      visit '/links'
 
-  expect(page.status_code).to eq 200
+      expect(page.status_code).to eq 200
 
-  scenario "user is able to save a link" do
-    expect(page).to have_text('http://google.com')
+      scenario "user is able to save a link" do
+        expect(page).to have_text('google')
+      end
+    end
   end
 end
